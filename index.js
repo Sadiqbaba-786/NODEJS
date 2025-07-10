81647fb0fc506d7cc33902742ee0149c2297eaaf
SadiqBaba <sadiqbaba192002@gmail.com>
2025-07-10 22:41:30 +0530

    hi

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-console.log("hello world");
-console.log("hi")
+
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Backend server is running');
+});
+
+app.get('/home', (req, res) => {
+  res.send('hi sadiq !');
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
